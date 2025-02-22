8ab6a2fee22d66bb4188652df8e2cf02d18cf944
SahitiSarva <sahiti.sarva@gmail.com>
2025-02-22 14:12:29 -0500

    Updates

---
+++ precache-manifest.37ae7454fc54cd115cff45d62a6cad2c.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e1f335fcd9b07727bb5d29e3937a961a",
+    "url": "/index.html"
+  },
+  {
+    "revision": "4a4452651f3e46a529e2",
+    "url": "/static/css/main.435d5fe5.chunk.css"
+  },
+  {
+    "revision": "638e200e69d950262938",
+    "url": "/static/js/2.ae026ab9.chunk.js"
+  },
+  {
+    "revision": "ea935f47a7b92aa14dc7",
+    "url": "/static/js/3.a29efab2.chunk.js"
+  },
+  {
+    "revision": "4a4452651f3e46a529e2",
+    "url": "/static/js/main.7bcd0661.chunk.js"
+  },
+  {
+    "revision": "cd2988c65fcca3ce3217",
+    "url": "/static/js/runtime~main.b45b9353.js"
+  }
+]);
