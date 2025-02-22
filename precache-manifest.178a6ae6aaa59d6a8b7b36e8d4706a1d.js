274b9e902d4ae1f04462346d2d438e87a86e4021
SahitiSarva <sahiti.sarva@gmail.com>
2025-02-22 14:44:31 -0500

    Updates

---
+++ precache-manifest.178a6ae6aaa59d6a8b7b36e8d4706a1d.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "9b18f7061daa4e6908dc1da5aaabb3a1",
+    "url": "/desinewyorkergames/index.html"
+  },
+  {
+    "revision": "4a4452651f3e46a529e2",
+    "url": "/desinewyorkergames/static/css/main.435d5fe5.chunk.css"
+  },
+  {
+    "revision": "638e200e69d950262938",
+    "url": "/desinewyorkergames/static/js/2.ae026ab9.chunk.js"
+  },
+  {
+    "revision": "ea935f47a7b92aa14dc7",
+    "url": "/desinewyorkergames/static/js/3.a29efab2.chunk.js"
+  },
+  {
+    "revision": "4a4452651f3e46a529e2",
+    "url": "/desinewyorkergames/static/js/main.7bcd0661.chunk.js"
+  },
+  {
+    "revision": "0611025d2c196008a07e",
+    "url": "/desinewyorkergames/static/js/runtime~main.53aba105.js"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.37ae7454fc54cd115cff45d62a6cad2c.js"
+  "/desinewyorkergames/precache-manifest.178a6ae6aaa59d6a8b7b36e8d4706a1d.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/desinewyorkergames/index.html"), {
   
   blacklist: [/^\/_/,/\/[^/]+\.[^/]+$/],
 });
